030b29180debf1e5a4492202d1741e6d9002510b
William Jose Gallerani <wjgallerani@saomanoel.com.br>
2021-09-15 20:10:33 -0300

    Atualização

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,29 +1,31 @@
 import { FaGithub } from 'react-icons/fa'
 import styles from './styles.module.scss';
-import { FiX } from 'react-icons/fi'
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ?
+    return session ?
         (
             <button
                 type="button"
                 className={styles.signInButton}
+                onClick={() => signOut()}
             >
                 <FaGithub color="#04d361" />
-                William Gallerani
+                {session.user.name}
                 <FiX color="#737380" className={styles.closeIcon} />
             </button>
         ) : (
             <button
                 type="button"
                 className={styles.signInButton}
+                onClick={() => signIn('github')}
             >
                 <FaGithub color="#eba417" />
                 Sign in with Github
             </button>
         );
-
 
 }
